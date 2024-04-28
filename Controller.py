19b749c52dcbc9fd6bf43e3593ee8a67c74b7fb4
David-Rino <rinodavid@nevada.unr.edu>
2024-04-27 22:37:30 -0700

    Reformatetd DataScraperModel to an actual class

--- Controller.py
+++ Controller.py
@@ -1,22 +1,23 @@
-import DataScraperModel as model
+from ScraperModel import DataScraper
 from PandasModel import PandasModel
 from View import View
 class Controller:
     def __init__(self, userView, pandasModel):
         self.userView = userView
         self.pandasModel = pandasModel
+        self.scraperModel = DataScraper()
 
     def loadDataFrame(self):
-        self.pandasModel.processFile(model.getOutputName())
+        self.pandasModel.processFile(self.scraperModel.xlsx_path)
 
     def runScraper(self):
-        model.main()
+        self.scraperModel.run()
 
     def runWindow(self):
         self.userView.openWindow()
 
     def setFilename(self, filename):
-        model.set_xlsx_file(filename)
+        self.scraperModel.set_xlsx_file(filename)
 
     def getDataFrame(self):
         return self.pandasModel.getDataFrame()
--- ScraperModel.py
+++ ScraperModel.py
@@ -1,103 +1,83 @@
 import os
 import time
-
 from openpyxl import load_workbook
-
 import undetected_chromedriver as uc
-
 import bs4
 
-class ScraperModel:
-
-    def __init__(self):
-        self.chromedriver_path = "chromedriver.exe"
-        self.xlsx_path = "data.xlsx"
-        self.CURR_SCRIPT_PATH = os.path.realpath(os.path.dirname(__file__))
-        self.profile_path = self.CURR_SCRIPT_PATH + "\\profile"
-
-        self.FIRST_NAME_COL = 'A'
-        self.LAST_NAME_COL = 'B'
-        self.ADDRESS_COL = 'K'
-        self.PHONEs_COLs = ['L', 'M', 'N', 'O', 'P']
+class DataScraper:
+    def __init__(self, chromedriver_path="chromedriver.exe", xlsx_path="data.xlsx"):
+        self.chromedriver_path = chromedriver_path
+        self.xlsx_path = xlsx_path
+        self.curr_script_path = os.path.realpath(os.path.dirname(__file__))
+        self.profile_path = self.curr_script_path + "\\profile"
+        self.first_name_col = 'A'
+        self.last_name_col = 'B'
+        self.address_col = 'F'
+        self.phones_cols = ['L', 'M', 'N', 'O', 'P']
 
     def open_chrome_with_profile(self):
         options = uc.ChromeOptions()
         options.add_argument("--user-data-dir=" + self.profile_path)
-
-        driver = uc.Chrome(driver_executable_path=self.chromedriver_path)
+        driver = uc.Chrome(driver_executable_path=self.chromedriver_path, options=options)
         return driver
 
     def set_xlsx_file(self, filename):
-        self.xlsx_path = filename
+        self.xlsx_path = filename.strip()
 
     def open_xlsx_file(self):
-        wb = load_workbook(filename = self.xlsx_path)
+        print(self.xlsx_path)
+        wb = load_workbook(self.xlsx_path)
         ws = wb.active
         return wb, ws
 
     def write_phones_to_xlsx_file(self, wb, ws, phones, row):
-
-        #Limit the amount of phone numbers to 5
-        phoneLength = len(phones)
-        if phoneLength >= 5:
-            phoneLength = 5
-
-        for i in range(phoneLength):
-            ws[self.PHONEs_COLs[i] + str(row)].value = phones[i]
-
+        phone_length = min(len(phones), 5)
+        for i in range(phone_length):
+            ws[self.phones_cols[i] + str(row)].value = phones[i]
         wb.save(self.xlsx_path)
 
     def extract_phones_from_page(self, page_source):
         phones = []
-
         try:
-            # Attempt to find phones
             soup = bs4.BeautifulSoup(page_source, "html.parser")
             a_tags = soup.find_all("a", title=lambda x: x and "Search people with phone number" in x)
             for a_tag in a_tags:
-                phone = a_tag.text.strip()
-                phones.append(phone)
-
-                return phones
-
+                phones.append(a_tag.text.strip())
         except Exception as e:
             print(str(e))
-            return phones
-
-    def getOutputName(self):
-        return self.xlsx_path
+        return phones
 
     def run(self):
         driver = self.open_chrome_with_profile()
         driver.get("https://www.fastpeoplesearch.com/")
-
         if "Access Denied" in driver.page_source:
-            print("Access Denied")
+            print("Access Denied, please enable VPN")
             time.sleep(60)
             driver.get("https://www.fastpeoplesearch.com/")
             if "Access Denied" in driver.page_source:
                 return 1
 
-            wb, ws = self.open_xlsx_file()
+        wb, ws = self.open_xlsx_file()
+        for row in range(2, ws.max_row + 1):
+            try:
+                first_name = ws[self.first_name_col + str(row)].value
+                last_name = ws[self.last_name_col + str(row)].value
+                address = ws[self.address_col + str(row)].value
 
-            for row in range(2, ws.max_row + 1):
-
-                try:
-                    first_name = ws[self.FIRST_NAME_COL + str(row)].value
-                    last_name = ws[self.LAST_NAME_COL + str(row)].value
-                    address = ws[self.ADDRESS_COL + str(row)].value
-
-                    if (first_name is None and last_name is None) or address is None:
-                        continue
-
-                    first_name = first_name.replace(" ", "-")
-                    last_name = last_name.replace(" ", "-")
-                    address = address.replace(" ", "-")
-                    driver.get("https://www.fastpeoplesearch.com/name/" + first_name + "-" + last_name + "-" + address)
-
-                except Exception as e:
-                    print(str(e))
+                if (first_name is None and last_name is None) or address is None:
                     continue
 
+                search_url = f"https://www.fastpeoplesearch.com/name/{first_name.replace(' ', '-')}-{last_name.replace(' ', '-')}_{str(address).replace(' ', '-')}"
+                driver.get(search_url)
+                phones = self.extract_phones_from_page(driver.page_source)
+                if phones:
+                    self.write_phones_to_xlsx_file(wb, ws, phones, row)
+                else:
+                    print(f"No phones found for {first_name} {last_name}")
+                time.sleep(1)
+            except Exception as e:
+                print(str(e))
+                continue
 
-
+        wb.close()
+        driver.close()
