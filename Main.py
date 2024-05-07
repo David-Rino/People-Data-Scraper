d4f52dc5b9b4858e09452f012c9099c71758d1e5
David-Rino <rinodavid@nevada.unr.edu>
2024-05-06 23:40:07 -0700

    Test

--- Main.py
+++ Main.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     userView = View(root)
     userDataScraper = DataScraper()
+    # Will have to change to the system's database connection
     userDatabase = SQLModel('localhost', 'DataScraper', 'postgres', 'Rd1258545', 5432)
     userController = Controller(userView, userData, userDatabase, userDataScraper)
     # Used so the View has a way to contact the controller during runtime
